35cc7005b451e28e090756c42a61009ac3b1f64d
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-29 19:35:50 +0200

    Add working reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,26 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { addContact, deleteContact } from './actions';
+import { nanoid } from 'nanoid';
+
 const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-export const contactsReducer = (state = contactsInitialState, action) => {};
+export const contactsReducer = createReducer(contactsInitialState, {
+  [addContact]: (state, action) => {
+    state.push(action.payload);
+  },
+  [deleteContact]: (state, action) => {
+    const index = state.findIndex(contact => contact.id === action.payload.id);
+    if (index !== -1) {
+      state.slice(index, 1);
+    }
+  },
+});
 
 const filtersInitialState = '';
 
-export const filtersReducer = (state = filtersInitialState, action) => {};
+export const filtersReducer = createReducer(filtersInitialState, {});
